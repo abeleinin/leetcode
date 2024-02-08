44842c534367add35fdb0fcc17b2f85ced5abce1
Abe Leininger <95333017+abeleinin@users.noreply.github.com>
2024-02-07 23:33:52 -0500

    leetcode 106 cpp

---
+++ neetcode150/easy/cpp/106-Maximum-Depth-of-Binary-Tree.cpp
@@ -0,0 +1,25 @@
+// Definition for a binary tree node.
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        int valLeft = 1 + maxDepth(root->left);
+        int valRight = 1 + maxDepth(root->right);
+        return max(valLeft, valRight);
+    }
+};
