f18868e191a121a9a043fdb26934c5897ba98f38
aleinin <95333017+abeleinin@users.noreply.github.com>
2024-07-12 23:32:35 -0400

    leetcode 11 cpp

--- neetcode150/medium/cpp/11-Container-With-Most-Water.cpp
+++ neetcode150/medium/cpp/11-Container-With-Most-Water.cpp
@@ -5,18 +5,20 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int i = 0, j = height.size() - 1;
-        int dist = 0; 
-        while(i < j) {
-            int right = height[i], left = height[j];
-            int curr = min(right, left) * (j - i);
-            dist = max(dist, curr);
-            if(right < left) {
-                i++;
+        int i = 0;
+        int j = height.size()-1;
+        int res = 0;
+        while (i < j){
+            int left = height[i];
+            int right = height[j];
+            int curr = min(left, right) * (j-i);
+            res = max(res, curr);
+            if (left > right){
+                --j;
             } else {
-                j--;
+                ++i;
             }
         }
-        return dist;
+        return res;
     }
 };
