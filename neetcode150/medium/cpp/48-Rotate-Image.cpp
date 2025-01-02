f7f599c17aedd87b65c17c9bab81b9606e8233ce
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-01-01 22:40:33 -0600

    leetcode 48 cpp

--- neetcode150/medium/cpp/48-Rotate-Image.cpp
+++ neetcode150/medium/cpp/48-Rotate-Image.cpp
@@ -13,3 +13,21 @@ public:
     }
   }
 };
+
+// Solution 2
+class Solution2 {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                if (j > i) {
+                    swap(matrix[i][j], matrix[j][i]);
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.size(); i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
