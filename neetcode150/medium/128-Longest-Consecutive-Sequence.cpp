08f02976186d6ec5590057491c306243a39bf001
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-01-21 18:21:33 -0600

    leetcode 128 notes

--- neetcode150/medium/128-Longest-Consecutive-Sequence.cpp
+++ neetcode150/medium/128-Longest-Consecutive-Sequence.cpp
@@ -3,7 +3,14 @@
 
 using namespace std;
 
-class Solution {
+/**
+ * \c Solution1
+ * 
+ * 1. Creates a set of the input vector.
+ * 2. Checks if the given number is the "beginning" of a sequence (ie num-1 is not contained in the set)
+ * 3. If it's the "beginning", count the sequence length using while loop
+ */
+class Solution1 {
 public:
     int longestConsecutive(vector<int>& nums) {
         set<int> s(nums.begin(), nums.end());
@@ -23,13 +30,19 @@ public:
     }
 };
 
-class Solution {
+/**
+ * \c Solution2
+ * 
+ * Same approach as \c Solution1, but uses std::set<>::contains, added in C++20
+ * https://en.cppreference.com/w/cpp/container/set/contains
+ */
+class Solution2 {
 public:
     int longestConsecutive(vector<int>& nums) {
         set<int> s(nums.begin(), nums.end());
 
         int res = 0;
-        for (auto num : s) {
+        for (int num : s) {
             if (!s.contains(num-1)) {
                 int curr = 1;
                 while (s.contains(num+1)) {
