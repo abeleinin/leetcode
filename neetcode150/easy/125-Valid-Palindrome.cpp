b548c3e8b0976d892017e78278c591d53d6045d9
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-01-21 18:27:02 -0600

    leetcode 125

--- neetcode150/easy/125-Valid-Palindrome.cpp
+++ neetcode150/easy/125-Valid-Palindrome.cpp
@@ -2,31 +2,70 @@
 
 using namespace std;
 
-// Solution 1
-class Solution {
+/**
+ * \c Solution1
+ * 
+ * 1. Check base cases (ie size == 0 || size == 1)
+ * 2. Create "clean" string without non-alphanumeric characters and uses lowercase character (ie isalnum and tolower utils)
+ * 3. Use two-pointer technique to compare beginning and end characters
+ */
+class Solution1 {
 public:
     bool isPalindrome(string s) {
-        int n = s.size();
-        
-        if(n == 1){
-            return 1;
+        if(s.size() == 1){
+            return true;
         }
         
-        string str = "";
+        string clean = "";
         for(char c : s){
             if(isalnum(c)){
-                str += tolower(c);
+                clean += tolower(c);
             }
         }
         
-        int i = 0, j = str.size() - 1;
+        int i = 0;
+        int j = clean.size() - 1;
         while(i < j){
-            if(str[i] != str[j]){
-                return 0;
+            if(clean[i] != clean[j]){
+                return false;
             }
             i++;
             j--;
         }
-        return 1;
+        return true;
+    }
+};
+
+/**
+ * \c Solution2
+ * 
+ * Similar to \c Solution1 but doesn't use auxiliary "clean" string, saving space.
+ */
+class Solution2 {
+public:
+    bool isPalindrome(string s) {
+        if (s.size() == 0 || s.size() == 1) {
+            return true;
+        }
+        
+        int i = 0;
+        int j = s.size()-1;
+        while(i < j) {
+            if (!isalnum(s[i])) {
+                ++i;
+                continue;
+            }
+
+            if (!isalnum(s[j])) {
+                --j;
+                continue;
+            }
+
+            if (tolower(s[i]) != tolower(s[j])) return false;
+            ++i;
+            --j;
+        }
+
+        return true;
     }
 };
