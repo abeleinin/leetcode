0aedb64be021b718540d2a4ff50d7f353438a191
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-01-22 20:55:07 -0600

    leetcode 567

--- neetcode150/medium/567-Permutation-in-String.cpp
+++ neetcode150/medium/567-Permutation-in-String.cpp
@@ -3,27 +3,64 @@
 
 using namespace std;
 
+/**
+ * \c Solution1
+ * 
+ * Sorting Approach:
+ * Sort input string and compare it to the sorted substr in s2
+ * 
+ * Time  : O(M * N log N)
+ * Space : O(N)
+ */
+class Solution1 {
+public:
+    bool checkInclusion(string s1, string s2) {
+        sort(s1.begin(), s1.end());
+
+        int i = 0;
+        int n = s1.size();
+        while ((i+n-1) < s2.size()) {
+            string substr = s2.substr(i, s1.size());
+            sort(substr.begin(), substr.end());
+            if (substr == s1) {
+                return true;
+            }
+            ++i;
+        }
+        return false;
+    }
+};
+
+/**
+ * \c Solution2
+ * 
+ * Optimal Approach
+ * 1. Store the character frequency of s1 and s2.substr(s1.size())
+ * 2. Use cool increment/decrement check approach 
+ * 
+ * Time  : O(N)
+ * Space : O(1)
+ */
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         if (s1.size() > s2.size()) return false;
-        
-        vector<int> v1(26, 0), v2(26, 0); 
 
-        for (int i = 0; i < s1.size(); i++) {
-            ++v1[s1[i] - 'a'];
-            ++v2[s2[i] - 'a'];
+        vector<int> s1Freq(26, 0);
+        vector<int> s2Freq(26, 0);
+        for (int i = 0; i < s1.size(); ++i) {
+            ++s1Freq[s1[i]-'a'];
+            ++s2Freq[s2[i]-'a'];
         }
 
-        if (v1 == v2) return true;
+        if (s1Freq == s2Freq) return true;
 
-        for (int j = s1.size(); j < s2.size(); j++) {
-            ++v2[s2[j] - 'a'];
-            --v2[s2[j - s1.size()] - 'a'];
+        for (int j = s1.size(); j < s2.size(); ++j) {
+            ++s2Freq[s2[j]-'a'];
+            --s2Freq[s2[j-s1.size()]-'a'];
 
-            if (v1 == v2) return true;
+            if (s1Freq == s2Freq) return true;
         }
-
         return false;
     }
 };
