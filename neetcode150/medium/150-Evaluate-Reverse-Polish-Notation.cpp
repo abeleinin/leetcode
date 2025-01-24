f058b10d342bc2fa748e01a32b42f9b378617b44
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-01-23 20:50:11 -0600

    leetcode 150

--- neetcode150/medium/150-Evaluate-Reverse-Polish-Notation.cpp
+++ neetcode150/medium/150-Evaluate-Reverse-Polish-Notation.cpp
@@ -3,33 +3,50 @@
 
 using namespace std;
 
+/**
+ * \c Solution
+ * 
+ * 1. Push numbers onto stack using std::stoi
+ * 2. Perform ops on top two numbers, push result back onto stack
+ * 3. return stack.top()
+ * 
+ * Time  : O(N)
+ * Space : O(N)
+ */
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int> my_stack;
+        stack<int> st;       
+
         for (string token : tokens) {
-            if (token == "+" || token == "-" || token == "*" || token == "/") {
-                int v1 = my_stack.top();
-                my_stack.pop();
-                int v2 = my_stack.top();
-                my_stack.pop();
-                if (token == "+") {
-                    my_stack.push(v2 + v1);
-                }
-                else if (token == "-") {
-                    my_stack.push(v2 - v1);
-                }
-                else if (token == "/") {
-                    my_stack.push(v2 / v1);
-                }
-                else if (token == "*") {
-                    my_stack.push(v2 * v1);
-                }
-            }
-            else {
-                my_stack.push(stoi(token));
+            if (token == "*") {
+                int n1 = st.top();
+                st.pop();
+                int n2 = st.top();
+                st.pop();
+                st.push(n2 * n1);
+            } else if (token == "-") {
+                int n1 = st.top();
+                st.pop();
+                int n2 = st.top();
+                st.pop();
+                st.push(n2 - n1);
+            } else if (token == "+") {
+                int n1 = st.top();
+                st.pop();
+                int n2 = st.top();
+                st.pop();
+                st.push(n2 + n1);
+            } else if (token == "/") {
+                int n1 = st.top();
+                st.pop();
+                int n2 = st.top();
+                st.pop();
+                st.push(n2 / n1);
+            } else {
+                st.push(stoi(token));
             }
         }
-        return my_stack.top();
+        return st.top();
     }
 };
