e126c1166c42cfed3ec531988d9bb51c8c25dc76
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-01-29 23:50:58 -0600

    leetcode 21

--- neetcode150/easy/21-Merge-Two-Sorted-Lists.cpp
+++ neetcode150/easy/21-Merge-Two-Sorted-Lists.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 
-// Definition for singly-linked list.
+/**
+ * Definition for singly-linked list.
+ */
 struct ListNode {
     int val;
     ListNode *next;
@@ -9,13 +11,21 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
  
-class Solution {
+/**
+ * \c Solution1
+ * 
+ * Recursive
+ * 
+ * Time  : O(n+m)
+ * Space : O(n+m)
+ */
+class Solution1 {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == NULL) {
+        if (list1 == nullptr) {
             return list2;
         }
-        if (list2 == NULL) {
+        if (list2 == nullptr) {
             return list1;
         }
         if (list1->val < list2->val) {
@@ -25,5 +35,51 @@ public:
             list2->next = mergeTwoLists(list1, list2->next);
             return list2;
         }
+    }
+};
+
+/**
+ * \c Solution2
+ * 
+ * Iterative
+ * 
+ * Time  : O(n+m)
+ * Space : O(1)
+ */
+class Solution2 {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if (list1 == nullptr) {
+            return list2;
+        } else if (list2 == nullptr) {
+            return list1;
+        }
+
+        ListNode* curr = list1;
+        if (list1->val > list2->val) {
+            curr = list2;
+            list2 = list2->next;
+        } else {
+            list1 = list1->next;
+        }
+        ListNode* ptr = curr;
+
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            } else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+            curr = curr->next;
+        }
+
+        if (list1 == nullptr) {
+            curr->next = list2;
+        } else {
+            curr->next = list1;
+        }
+        return ptr;
     }
 };
