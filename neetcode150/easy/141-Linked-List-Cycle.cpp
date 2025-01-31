2ebb109ca7702f3afe24562fab3c78db88a5241f
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-01-30 21:55:22 -0600

    leetcode 141

--- neetcode150/easy/141-Linked-List-Cycle.cpp
+++ neetcode150/easy/141-Linked-List-Cycle.cpp
@@ -1,21 +1,33 @@
 #include <algorithm>
 
-//  Definition for singly-linked list.
+/**
+ * Definition for singly-linked list.
+ */
 struct ListNode {
     int val;
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
  
+ /**
+  * \c Solution
+  * 
+  * 1. Use fase and slow pointer
+  * 2. while fast and fast->next so that you can call fast->next->next inside the loop
+  * 
+  * Time  : O(n)
+  * Space : O(1)
+  */
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (head == NULL) {
+        if (head == nullptr) {
             return false;
         }
+
         ListNode* slow = head;
         ListNode* fast = head->next;
-        while (fast != NULL && fast->next != NULL) {
+        while (fast != nullptr && fast->next != nullptr) {
             if (slow == fast) {
                 return true;
             }
