fcdc6eabdac224bd7e80885c0fb20ee8ec3c10ae
aleinin <95333017+abeleinin@users.noreply.github.com>
2025-02-04 23:58:39 -0600

    linked list methods wip

--- data-structures/src/linked_list.cc
+++ data-structures/src/linked_list.cc
@@ -18,3 +18,23 @@ LinkedList<T>::LinkedList(T value) {
     tail = head;
     ++size;
 }
+
+template <typename T>
+LinkedList<T>::add(T value) {
+    head->next = value;
+    head = &value;
+    ++size;
+}
+
+void LinkedList::print_list() const {
+    if (size == 0) {
+        std::cout << "nullptr" << std::endl;
+    }
+
+    std::cout << "{ " << head->value;
+    for (int i = 1; i < size; ++i) {
+        ++head;
+        std::cout << " -> " << head->value
+    }
+    std::cout << " -> nullptr " << head->value;
+}
